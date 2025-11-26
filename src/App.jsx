b8ef13352704caa5946f7abd499c21889209afc0
caua-lucas <caual5397@gmail.com>
2025-11-26 12:28:02 -0300

    Aprendendo React Hook- Use Ref

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { Container, Image, ContainerItens, H1, InputLabel, Input, Button, User  } from './styles';
+import React, { useState, useRef } from 'react'
+import { Container, Image, ContainerItens, H1, InputLabel, Input, Button, User } from './styles';
 import People from './assets/people.svg';
 import Seta from "./assets/arrow.svg";
 import Lixeira from './assets/trash.svg'
@@ -8,50 +8,43 @@ import Lixeira from './assets/trash.svg'
 function App() {
 
 
-    const [users, setUsers] = useState([])
-    const [name, setName] = useState("")
-    const [age, setAge] = useState("")
-
-    function addNewUser(){
-      setUsers([...users,{id:Math.random(),name,age}]) //spreed operator(...users)(itens anteriores) + novo item
+  const [users, setUsers] = useState([])
+  const inputName = useRef()
+  const inputAge = useRef()
 
 
-      console.log(users)
-    }
+  function addNewUser() {
+    setUsers([...users, { id: Math.random(), name: inputName.current.value, age: inputAge.current.value }]) //spreed operator(...users)(itens anteriores) + novo item
 
-    function changeInputName(event) {
-      setName(event.target.value)
 
-    }
+    console.log(users)
+  }
 
-        function changeInputAge(event) {
-      setAge(event.target.value)
 
-    }
   return (
     <>
-    <Container>
-      <Image alt="logo-imagem" src={People} />
-      <ContainerItens>
-        <H1>Olá!</H1>
+      <Container>
+        <Image alt="logo-imagem" src={People} />
+        <ContainerItens>
+          <H1>Olá!</H1>
 
-        <InputLabel>Nome</InputLabel>
-        <Input onChange={changeInputName} placeholder="Nome" />
+          <InputLabel>Nome</InputLabel>
+          <Input ref={inputName} placeholder="Nome" />
 
-        <InputLabel >Idade</InputLabel>
-        <Input onChange={changeInputAge} placeholder="Idade" />
-        <Button onClick={addNewUser} >Cadastrar <img alt="imagem" src={Seta}/></Button>
-        <ul>
-          {users.map((user) => (
-            <User key={user.id}>
-              <p>{user.name}</p> <p> {user.age}</p>
-              <button><img src={Lixeira} alt="lixeira"/></button>
-            </User>
-          ))
-          }
-        </ul>
-      </ContainerItens>
-    </Container>
+          <InputLabel >Idade</InputLabel>
+          <Input ref={inputAge} placeholder="Idade" />
+          <Button onClick={addNewUser} >Cadastrar <img alt="imagem" src={Seta} /></Button>
+          <ul>
+            {users.map((user) => (
+              <User key={user.id}>
+                <p>{user.name}</p> <p> {user.age}</p>
+                <button><img src={Lixeira} alt="lixeira" /></button>
+              </User>
+            ))
+            }
+          </ul>
+        </ContainerItens>
+      </Container>
     </>
   );
 }
